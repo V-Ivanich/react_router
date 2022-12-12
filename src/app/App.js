2a720d9a5f70827d89f4c648d0b289b35de05a32
Vasilii <84954523+V-Ivanich@users.noreply.github.com>
2022-12-12 22:44:31 +0300

    chenges routing

--- src/app/App.js
+++ src/app/App.js
@@ -2,9 +2,17 @@ import NavBar from "./components/navBar";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./components/dashboard";
 import Login from "./components/login";
-import Posts from "./components/posts";
+// import Posts from "./components/posts";
 import Home from "./components/home";
 import Stats from "./components/stats";
+import PostsList from "./components/postsList";
+import Post from "./components/post";
+
+const posts = [
+  { id: 1, label: "post 1"},
+  { id: 2, label: "post 2"},
+  { id: 3, label: "post 3"}
+];
 
 function App() {
   return (
@@ -13,12 +21,10 @@ function App() {
       <h1>App</h1>
       <Switch>
         <Route path="/dashboard/stats" component={Stats} />
-        <Route
-          path="/dashboard"
-          render={(props) => <Dashboard isAdmin={false} {...props} />}
-        />
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="/login" component={Login} />
-        <Route path="/posts" component={Posts} />
+        <Route path="/posts/:postsId" render={(props) => (<Post posts={posts} {...props} />)} />
+        <Route path="/posts" component={PostsList} />
         <Route path="/" exact component={Home} />
       </Switch>
     </div>
